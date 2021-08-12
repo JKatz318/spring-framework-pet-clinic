2dc591ec62110532ccf426f08b877163a90c85cd
Jeremy <Jeremy@keepers.ai>
2021-08-12 21:13:20 +0300

    Initial project add

---
+++ src/test/java/jkatz/guru/springframework/springframeworkpetclinic/SpringFrameworkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jkatz.guru.springframework.springframeworkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
